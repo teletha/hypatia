88b42f4590d4ed1172ddd1d00673faa853a952cd
teletha <teletha@users.noreply.github.com>
2024-03-29 02:36:12 +0000

    deploy: 642e70db8644664ce063c706002c1d5001f14832

---
+++ root.js
@@ -0,0 +1,37 @@
+const root = {
+	"docs": [],
+	"modules": [],
+	"packages": [
+		"hypatia"
+	],
+	"types": [
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Arithmetic",
+			"packageName": "hypatia",
+			"type": "AbstractClass"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Num",
+			"packageName": "hypatia",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Orientational",
+			"packageName": "hypatia",
+			"type": "Functional"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Primitives",
+			"packageName": "hypatia",
+			"type": "Class"
+		}
+	]
+}
